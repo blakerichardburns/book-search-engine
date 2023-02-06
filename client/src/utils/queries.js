a6f657b3a82751ddf69fb2b8e5dd866c84c971e3
Blake Burns <bburns@mysgi.com>
2023-02-05 23:25:43 -0500

    added mutations.js content, updated queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,21 @@
-// •	queries.js: This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-    query me {
-        me {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                _id
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
+  }
 `;
